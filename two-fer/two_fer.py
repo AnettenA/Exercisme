3c9e5c24ad94363c2bc0298ad411fd21c61f95bc
AleksandraPytlarz <aleksandrapytlarz@gmail.com>
2020-09-08 14:30:38 +0200

    Lepsze rozwiązanie

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,6 +1,6 @@
-def two_fer(name=None):
-        if name is None:
-                return('One for you, one for me.')
-        else:
-                return(f'One for {name}, one for me.')
-        pass
+def two_fer(name = 'you'):
+        return(f'One for {name}, one for me.')
+pass
+
+print(two_fer("Mat"))
+
